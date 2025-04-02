668be79c50aeb82d7d2cbdd2645a787cedc3c6cf
SonamDM <sonamm89.sm@gmail.com>
2025-04-02 20:46:33 +0800

    This is workshop 1 v3

--- workshee1.py
+++ workshee1.py
@@ -1,4 +1,24 @@
+#Question 1
 a=15
 b=12
 print(type(a))
-print(type(b))
+print(type(b)) 
+#Question 2
+a=15
+b=12
+result=a+b
+print(result)
+result=a-b
+print(result)
+result=a*b
+print(result)
+result=a/b
+print(result)
+#Question 3
+c=a//b
+print(type(c))
+print(float(c))
+#Question 4
+message="The result of a divided by b is:"
+result=message+str(c)
+print(result)
